55d48f5e7c25b4406ff3294b0601d8ee82b46c4c
FunnyNameHaha <anton.bilchinski@googlemail.com>
2018-05-27 10:40:59 +0200

    bug fixes

---
+++ src/pages/searcherror/searcherror.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SearcherrorPage } from './searcherror';
+
+@NgModule({
+  declarations: [
+    SearcherrorPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SearcherrorPage),
+  ],
+})
+export class SearcherrorPageModule {}
