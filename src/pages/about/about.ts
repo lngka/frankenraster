6f9ebc9793b4d8a25af519044a07332be8db2166
lngka <foxlead@gmail.com>
2018-06-17 20:56:21 +0200

    able to view user orders

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,70 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FlashProvider } from '../../providers/flash/flash';
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
+  orders = [];
+  statuses = [];
+  loginID : Number;
+  constructor(public navCtrl: NavController, private flash: FlashProvider, private http: HttpClient, private storage: Storage) {
 
   }
+  // update statuses everytime before page loads
+  ionViewWillEnter() {
+    this.storage.get("status")
+      .then((d) => {
+        // the 1st element in statuses must be set to support the logic in .html
+        this.statuses[0] = {
+          StatusID: 0,
+          Status: "NULL",
+          VerbietetAktion: []
+        };
+        var data = JSON.parse(d);
+        data.forEach((item) => {
+          item.VerbietetAktion = this.verbietetAktionToArray(item.VerbietetAktion);
+          this.statuses[parseInt(item.StatusID)] = item;
+        });
+      })
+      .catch((e) => {
+        console.error(e);
+        return this.flash.show(e, 3000);
+      });
+  }
 
+  // update orders everytime before page loads
+  ionViewDidEnter() {
+    this.storage.get("loginData")
+      .then(d => {
+        var data = JSON.parse(d);
+        this.loginID = data["LoginID"];
+
+        let myURL = "https://frdb-lngka.c9users.io/getorders?loginID="+this.loginID;
+        this.http.get(myURL)
+          .subscribe(
+            (data) => {
+              this.orders = JSON.parse(JSON.stringify(data));
+              console.log(JSON.stringify(data));
+              console.log(myURL);
+            },
+            error => {
+              console.error(error);
+              return this.flash.show(error, 3000);
+            }
+          )
+      })
+      .catch(e => {
+        console.error(e);
+        return this.flash.show(e, 3000);
+      })
+  }
+
+  verbietetAktionToArray(string) {
+    return string.split("|").filter(Number);
+  }
 }
